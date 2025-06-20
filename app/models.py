bc762f4ff3d15ffe29902e7e547f5d81970a65c9
NHelixo <167092094+NHelixo@users.noreply.github.com>
2025-06-20 19:20:36 +0300

    +

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class UserRole(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    role = models.ForeignKey(UserRole, on_delete=models.SET_NULL, null=True)
---
+++ main.py
@@ -0,0 +1,20 @@
+import django
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projec.settings")
+django.setup()
+
+from app.models import User, UserRole
+
+#UserRole(
+#    name = "user"
+#).save()
+
+#User(
+#    name = "Arsen",
+#    age = "18",
+#    email = "oleksandr@gmail.com",
+#    role = UserRole.objects.get(name = "user")
+#).save()
+
+
